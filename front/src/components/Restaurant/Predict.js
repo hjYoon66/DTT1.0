a74bd032a57b8fd8baf144539c971ea7e5b66ef7
Jino Son <sojino0813@naver.com>
2023-10-16 13:23:22 +0900

    add predict css

--- front/src/components/Restaurant/Predict.js
+++ front/src/components/Restaurant/Predict.js
@@ -1,44 +1,119 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
+import React, {useEffect, useState} from "react";
+import NavBar2 from "../Nav/NavBar2";
 
-const API_KEY = "94663c62d6a8d0293add18abca03af44";
 const Predict = () => {
-    useEffect(() => {
-        navigator.geolocation.getCurrentPosition((position) => {
-            let lat = position.coords.latitude;
-            let lon = position.coords.longitude;
-            console.log(lat);
-            console.log(lon);
-            getWeather(lat, lon);
-        });
-    }, []);
+    const dateNow = new Date();
+    const todayDate = dateNow.toISOString().slice(0, 10);
+    const [date, setDate] = useState(todayDate);
+    const [time, setTime] = useState("");
+    const [table, setTable] = useState("");
+    const [predict, setPredict] = useState("");
 
+    const currentDate = new Date();
+    const oneWeekAgo = new Date(currentDate.getTime() + 7 * 24 * 60 * 60 * 1000);
+    const futureDate = oneWeekAgo.toISOString().split("T")[0];
+    const handleFormSubmit = async (e) => {
+        e.preventDefault();
 
-
-    const getWeather = async (lat, lon) => {
         try {
-            const res = await fetch(
-                `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}`
+            // 이미 예약된 데이터가 있는지 확인하는 요청
+            const response = await fetch(
+                `/reservation/data?date=${date}&time=${time}&tableN=${table}`,
+                {
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                }
             );
-            const json = await res.json();
-            const city = json.city.name;
-            const temp = json.list[0].main.temp //[] 안의 값은 숫자 0 ~ 40까지 -> 변해야함
-            const wind = json.list[0].wind.speed;
-            const rain = json.list[0].rain; //rain인경우가 없음 현재
-            console.log(json);
-            console.log(temp)
-            console.log(city)
-            console.log(wind)
-            console.log(rain)
-        } catch (err) {
-            console.error(err);
+            if (response.ok) {
+                const data = await response.json();
+                console.log("ok");
+                setPredict(data);
+                const resultContainer = document.getElementById("resultContainer");
+                resultContainer.innerText = data; // 결과 값을 텍스트로 삽입
+            } else {
+                console.log("error");
+            }
+        } catch (error) {
+            console.error("오류가 발생하였습니다.", error);
         }
     };
+
+    function generateHourOptions() {
+        const options = [];
+        const startTime = 11; // 시작 시간 (9시)
+        const endTime = 20; // 종료 시간 (18시)
+
+        for (let hour = startTime; hour <= endTime; hour++) {
+            for (let minute = 0; minute <= 59; minute += 60) {
+                const formattedHour = hour.toString().padStart(2, "0"); // 시간을 2자리 숫자로 포맷팅
+                const formattedMinute = minute.toString().padStart(2, "0"); // 분을 2자리 숫자로 포맷팅
+                const time = `${formattedHour}:${formattedMinute}`;
+                options.push(
+                    <option key={time} value={time}>
+                        {time}
+                    </option>
+                );
+            }
+        }
+
+        return options;
+    }
+
     return (
         <>
-            <h2>hello</h2>
+            <NavBar2/>
+            <div className="PredictInner-container">
+                <div className="Predict-container">
+                    <h2 className="Predict-title">예측하기</h2>
+                </div>
+                <form className="Predict-form" onSubmit={handleFormSubmit}>
+                    <select
+                        className="Predict-input"
+                        value={table === "1" ? "table1" : "table2"}
+                        onChange={(e) => {
+                            const value = e.target.value === "table1" ? "1" : "2";
+                            setTable(value);
+                        }}>
+                        <option value="">테이블 선택</option>
+                        <option value="table1">Table 1</option>
+                        <option value="table2">Table 2</option>
+                    </select>
+                    <input
+                        className="Predict-input"
+                        type="date"
+                        value={date}
+                        min={todayDate}
+                        onChange={
+                            (e) =>
+                                setDate(
+                                    e.target.value >= todayDate && e.target.value <= futureDate
+                                        ? e.target.value
+                                        : todayDate
+                                )
+                            // setDate((e.target.value >= todayDate) && (e.target.value <=todayDate-) ? e.target.value : todayDate)
+                        }
+                    />
+                    <select
+                        className="Predict-input"
+                        type="time"
+                        value={time}
+                        onChange={(e) => setTime(e.target.value)}>
+                        <option value="">시간 선택</option>
+                        {generateHourOptions()}
+                    </select>
+                    <button className="Predict-submit" type="submit">
+                        예측 하기
+                    </button>
+                </form>
+                {predict === "" ? (
+                    <></>
+                ) : (
+                    <div className="Predict-result">{predict}% 확률로 예약이 있습니다.</div>
+                )}
+            </div>
         </>
     );
 };
-
 export default Predict;
